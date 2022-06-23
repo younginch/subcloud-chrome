4537994588f051932506ee0441b841017ca8be8c
Seungbin Oh <sboh1214@gmail.com>
2022-06-23 21:52:34 +0900

    Add jest test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # subcloud-chrome
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/5ce045c808ff9cf507c3/maintainability)](https://codeclimate.com/repos/62b4583a044d754f9b000137/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/5ce045c808ff9cf507c3/test_coverage)](https://codeclimate.com/repos/62b4583a044d754f9b000137/test_coverage)
+
 subcloud for chrome extension
---
+++ jest.config.js
@@ -0,0 +1,28 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          target: 'es2021',
+        },
+        sourceMaps: true,
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/*.d.ts',
+  ],
+};
+
+module.exports = config;
