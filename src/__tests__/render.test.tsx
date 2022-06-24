e00893a054ece2cd1d1e574e92eb345fa780f0a0
Seungbin Oh <sboh1214@gmail.com>
2022-06-24 10:57:55 +0900

    fix test

--- src/__tests__/render.test.tsx
+++ src/__tests__/render.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import React from 'react';
-import App from '../pages/content/app';
-import Newtab from '../pages/newtab/newtab';
+import App from '../pages/content/App';
+import Newtab from '../pages/newtab/Newtab';
 
 describe('Pages', () => {
   it('renders Content', async () => {
