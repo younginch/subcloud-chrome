83e59b9614594aab3b5d7e1e82f1bde4a4529dc6
Seungbin Oh <sboh1214@gmail.com>
2022-06-24 11:01:08 +0900

    add some tests

--- src/__tests__/render.test.tsx
+++ src/__tests__/render.test.tsx
@@ -2,10 +2,20 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import App from '../pages/content/App';
 import Newtab from '../pages/newtab/Newtab';
+import Options from '../pages/options/Options';
+import Panel from '../pages/panel/Panel';
 
 describe('Pages', () => {
   it('renders Content', async () => {
     render(<App />);
+  });
+
+  it('renders Options', async () => {
+    render(<Options title="" />);
+  });
+
+  it('renders Panel', async () => {
+    render(<Panel />);
   });
 
   it('renders Popup', async () => {
