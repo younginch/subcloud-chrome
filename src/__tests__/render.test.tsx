29a04ecf9223a6185d508cd85f24c57126667ec4
Seungbin Oh <sboh1214@gmail.com>
2022-06-27 10:59:48 +0900

    add jest spyOn mocking

--- src/__tests__/render.test.tsx
+++ src/__tests__/render.test.tsx
@@ -4,9 +4,16 @@ import Newtab from '../pages/Newtab/Newtab';
 import Options from '../pages/Options/Options';
 import Panel from '../pages/Panel/Panel';
 import Popup from '../pages/Popup/Popup';
+import * as GetTab from '../pages/Content/utils/getTab';
 
 describe('Pages and Components', () => {
+  beforeAll(() => {
+    jest.spyOn(GetTab, 'default').mockResolvedValue({});
+  });
+
   it('renders controller', async () => {
+    jest.spyOn(GetTab, 'default').mockResolvedValue({});
+
     render(<Controller />);
   });
 
