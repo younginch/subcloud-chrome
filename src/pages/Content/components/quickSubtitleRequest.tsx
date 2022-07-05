d16ea16a8563bf9e42923bb80ec53ba5482f5797
Mingyu Lee <minkyu.lee65@gmail.com>
2022-07-05 14:07:00 +0900

    add footer message

--- src/pages/Content/components/quickSubtitleRequest.tsx
+++ src/pages/Content/components/quickSubtitleRequest.tsx
@@ -24,57 +24,79 @@ export default function QuickSubtitleRequest() {
     if (Math.random() < 0.1) setDefaultRequestLang('Kr');
   }, []);
 
-  return defaultRequestLang ? (
-    <Button colorScheme="pink">Request Subtitle</Button>
-  ) : (
-    <Popover placement="bottom">
-      <PopoverTrigger>
-        <Button colorScheme="pink">Request Subtitle</Button>
-      </PopoverTrigger>
-      <PopoverContent
-        color="white"
-        bg="blue.800"
-        borderColor="blue.800"
-        w="300px"
-        h="205px"
+  return (
+    <Stack>
+      {defaultRequestLang ? (
+        <Button colorScheme="blue" variant="outline" fontSize="14px" h="30px">
+          Request Subtitle
+        </Button>
+      ) : (
+        <Popover placement="bottom">
+          <PopoverTrigger>
+            <Button
+              colorScheme="blue"
+              variant="outline"
+              fontSize="14px"
+              h="30px"
+            >
+              Request Subtitle
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent
+            color="white"
+            bg="blue.800"
+            borderColor="blue.800"
+            w="300px"
+            h="205px"
+          >
+            <PopoverHeader pt={4} fontWeight="bold" border="0" fontSize="17px">
+              무료 자막 요청
+            </PopoverHeader>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <PopoverBody>
+              <Stack alignItems="center">
+                <Text fontSize="16px">자막을 요청할 언어를 선택하세요.</Text>
+                <SelectLang
+                  width="140px"
+                  height="30px"
+                  mainFont="13px"
+                  subFont="11px"
+                  clickEvent={() => null}
+                  marginTop="10px !important"
+                />
+                <Text fontSize="14px" color="gray.300">
+                  기본 요청 언어를 선택하면 앞으로 클릭 한번으로 요청할 수
+                  있습니다.
+                </Text>
+              </Stack>
+            </PopoverBody>
+            <PopoverFooter
+              border="0"
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+              pb={4}
+              mt="4px !important"
+            >
+              <Box className="default-language-small">
+                <Checkbox defaultChecked>기본 요청 언어로 저장</Checkbox>
+              </Box>
+              <Button colorScheme="blue" fontSize="15px" w="100px" h="30px">
+                요청 전송
+              </Button>
+            </PopoverFooter>
+          </PopoverContent>
+        </Popover>
+      )}
+      <Text
+        fontSize="12px"
+        color="gray.500"
+        mt="0px !important"
+        textAlign="center"
       >
-        <PopoverHeader pt={4} fontWeight="bold" border="0" fontSize="17px">
-          무료 자막 요청
-        </PopoverHeader>
-        <PopoverArrow />
-        <PopoverCloseButton />
-        <PopoverBody>
-          <Stack alignItems="center">
-            <Text fontSize="16px">자막을 요청할 언어를 선택하세요.</Text>
-            <SelectLang
-              width="140px"
-              height="30px"
-              mainFont="13px"
-              subFont="11px"
-              clickEvent={() => null}
-              marginTop="10px !important"
-            />
-            <Text fontSize="14px" color="gray.300">
-              기본 요청 언어를 선택하면 앞으로 클릭 한번으로 요청할 수 있습니다.
-            </Text>
-          </Stack>
-        </PopoverBody>
-        <PopoverFooter
-          border="0"
-          display="flex"
-          alignItems="center"
-          justifyContent="space-between"
-          pb={4}
-          mt="4px !important"
-        >
-          <Box className="default-language-small">
-            <Checkbox defaultChecked>기본 요청 언어로 저장</Checkbox>
-          </Box>
-          <Button colorScheme="blue" fontSize="15px" w="100px" h="30px">
-            요청 전송
-          </Button>
-        </PopoverFooter>
-      </PopoverContent>
-    </Popover>
+        Powered by SubCloud
+      </Text>
+    </Stack>
   );
 }
