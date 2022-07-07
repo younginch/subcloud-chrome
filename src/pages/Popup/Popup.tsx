2b422a5356a7539de1d436c540c2a07d79ed0785
Mingyu Lee <minkyu.lee65@gmail.com>
2022-07-07 15:25:41 +0900

    impl popup

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,23 +1,89 @@
-import logo from '../../assets/runtime/logo.svg';
-import './Popup.css';
+import {
+  Button,
+  Heading,
+  HStack,
+  Link,
+  Stack,
+  Text,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { BsMoonStarsFill, BsSun } from 'react-icons/bs';
+import LoginFirst from './components/loginFirst';
 
 export default function Popup() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const linkColor = useColorModeValue('blue', 'blue.400');
+  const login = true;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Stack alignItems="center" spacing="20px" p="15px">
+      <HStack>
+        <Heading>SubCloud</Heading>
+        <Button
+          aria-label="Toggle Color Mode"
+          onClick={toggleColorMode}
+          _focus={{ boxShadow: 'none' }}
+          w="fit-content"
+          bg="transparent"
         >
-          Learn React!
-        </a>
-      </header>
-    </div>
+          {colorMode === 'light' ? <BsMoonStarsFill /> : <BsSun />}
+        </Button>
+      </HStack>
+      {login ? (
+        <>
+          <HStack fontSize="15px">
+            <Text>로그인 상태:</Text>
+            <Text>갓띵건</Text>
+            <Button
+              fontSize="12px"
+              h="fit-content"
+              p="5px 10px 5px 10px"
+              onClick={() =>
+                chrome.tabs.create({ url: `${API_URL}/api/auth/signout` })
+              }
+            >
+              로그아웃
+            </Button>
+          </HStack>
+          <HStack fontSize="15px">
+            <Text>보유 포인트:</Text>
+            <Text>0</Text>
+            <Button
+              fontSize="12px"
+              h="fit-content"
+              p="5px 10px 5px 10px"
+              onClick={() => chrome.tabs.create({ url: `${API_URL}/buy` })}
+            >
+              포인트 충전
+            </Button>
+          </HStack>
+          <HStack>
+            <Button colorScheme="blue" p="10px" fontSize="14px" h="fit-content">
+              공개 프로필
+            </Button>
+            <Button
+              colorScheme="red"
+              p="10px"
+              fontSize="14px"
+              h="fit-content"
+              onClick={() => chrome.tabs.create({ url: `${API_URL}/user/my` })}
+            >
+              개인 프로필
+            </Button>
+          </HStack>
+          <Text
+            as={Link}
+            fontSize="14px"
+            color={linkColor}
+            onClick={() => chrome.tabs.create({ url: API_URL })}
+          >
+            웹페이지 방문
+          </Text>
+        </>
+      ) : (
+        <LoginFirst />
+      )}
+    </Stack>
   );
 }
--- src/pages/Popup/index.tsx
+++ src/pages/Popup/index.tsx
@@ -2,8 +2,26 @@ import { createRoot } from 'react-dom/client';
 
 import Popup from './Popup';
 import './index.css';
+// eslint-disable-next-line import/order
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  initialColorMode: 'light',
+  colors: {
+    bgColor: {
+      200: '#26303E',
+      300: '#232C39',
+      500: '#1A202C',
+      800: '#1C1E21',
+    },
+  },
+});
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-createRoot(window.document.querySelector('#app-container')!).render(<Popup />);
+createRoot(window.document.querySelector('#app-container')!).render(
+  <ChakraProvider theme={theme}>
+    <Popup />
+  </ChakraProvider>
+);
 
 if (module.hot) module.hot.accept();
